a9f2b1c6529e7f9a4365a45c27b3901704f1dbf0
Hussien <Adienbob@yahoo.com>
2025-05-06 16:18:38 +0300

    Editing project title

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,7 @@ import Logo from "./components/logo"
 import Header from "./components/header"
 import Buttons from "./components/buttons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Head from "next/head"
 import {
   faFacebookF,
   faTwitter,
@@ -16,32 +17,38 @@ export default function Home() {
 
 
   return (
-    <div className="container">
-      <section>
-        <Logo />
-        <Header />
-        <p className="subscribe">Subscribe and get notified</p>
-        <Buttons />
-        <img
-          src="/illustration-dashboard.png"
-          alt="Dashboard illustration"
-          className="dashboard"
-        />
-      </section>
-      <footer>
-        <div className="icons">
-          <a href="#">
-            <FontAwesomeIcon className="icon" icon={faFacebookF} />
-          </a>
-          <a href="#">
-            <FontAwesomeIcon className="icon" icon={faTwitter} />
-          </a>
-          <a href="#">
-            <FontAwesomeIcon className="icon" icon={faInstagram} />
-          </a>
-        </div>
-        <p>&copy; Copyright Ping. All rights reserved.</p>
-      </footer>
-    </div>
+    <>
+      <Head>
+        <title>Ping single column coming soon page</title>
+      </Head>
+      <div className="container">
+        <section>
+          <Logo />
+          <Header />
+          <p className="subscribe">Subscribe and get notified</p>
+          <Buttons />
+          <img
+            src="/illustration-dashboard.png"
+            alt="Dashboard illustration"
+            className="dashboard"
+          />
+        </section>
+        <footer>
+          <div className="icons">
+            <a href="#">
+              <FontAwesomeIcon className="icon" icon={faFacebookF} />
+            </a>
+            <a href="#">
+              <FontAwesomeIcon className="icon" icon={faTwitter} />
+            </a>
+            <a href="#">
+              <FontAwesomeIcon className="icon" icon={faInstagram} />
+            </a>
+          </div>
+          <p>&copy; Copyright Ping. All rights reserved.</p>
+        </footer>
+      </div>
+    </>
+
   );
 }
